75984d498da83c6460016f811f6d281a69869e1a
baboso <jjatria@gmail.com>
2015-03-22 17:36:39 +0000

    Added POD to cpran and CPrAN.pm

--- scripts/CPrAN.pm
+++ scripts/CPrAN.pm
@@ -4,10 +4,38 @@ use App::Cmd::Setup -app;
 use File::Path;
 # use Getopt::Long;
 
+=encoding utf8
+
+=head1 NAME
+
+B<CPrAN> - A package manager for Praat
+
+=head1 SYNOPSIS
+
+    use CPrAN;
+    CPrAN->run;
+
+=cut
+
+our $VERSION = '0.0.1';
+
 our $ROOT  = "../.cpran";
 our $PRAAT = "../..";
 our $TOKEN = 'WMe3t_ANxd3yyTLyc7WA';
-our $VERSION = '0.0.1';
+
+=head1 DESCRIPTION
+
+B<CPrAN> is the parent class for an App::Cmd application to search, install, remove
+and update Praat plugins.
+
+As a App::Cmd application, use of this module is separated over a number of
+different files. The main script invokes the root module and executes it, as in
+the example given in the SYNOPSIS.
+
+B<CPrAN> commands (inhabiting the B<CPrAN::Command> namespace) can call the
+methods described below to perform general B<CPrAN>-related actions.
+
+=cut
 
 # GetOptions (
 #   'praat=s' => \$PRAAT,
@@ -17,8 +45,30 @@ our $VERSION = '0.0.1';
 
 File::Path::make_path( $ROOT );
 
+=head1 METHODS
+
+=cut
+
+=over
+
+=item is_cpran()
+
+Takes an object of type Path::Class and checks whether it is a B<CPrAN> Praat
+plugin. See I<is_plugin()> for the criteria they need to fulfill ot be a plugin.
+
+In order to be considered a B<CPrAN> plugin, a valid plugin must additionally have
+a I<plugin descriptor> written in valid YAML.
+
+This method does not currently make any sanity checks on the structure of the
+plugin descriptor (which should follow the example bundled in I<example.yaml>),
+but future versions might.
+
+=cut
 sub is_cpran {
   my ($opt, $arg) = @_;
+
+  die "Argument is not a Path::Class object"
+    unless (ref $arg eq 'Path::Class');
 
   use YAML::XS;
   use File::Slurp;
@@ -39,23 +89,52 @@ sub is_cpran {
   return 1;
 }
 
+=item is_plugin()
+
+Takes an object of type Path::Class and checks whether it is a Praat plugin. All
+directories that reside under Praat's preferences directory, and whose name
+begins with the I<plugin_> identifier are considered valid plugins.
+
+    use Path::Class;
+    is_plugin( file('foo', 'bar') );           # False
+    is_plugin( dir('foo', 'bar') );            # False
+    is_plugin( dir($prefdir, 'bar') );         # False
+    is_plugin( dir($prefdir, 'plugin_bar') );  # True
+
+=cut
 sub is_plugin {
   my ($opt, $arg) = @_;
+
+  die "Argument is not a Path::Class object"
+    unless (ref $arg eq 'Path::Class');
 
   unless ($arg->is_dir) {
     print STDERR "D: ", $arg->basename, " is not a directory\n" if $opt->{debug};
     return 0;
   }
 
-  if ($arg->basename =~ /^plugin_/) {
-    return 1;
-  }
-  else {
-    print STDERR "D: ", $arg->basename, " is not a plugin\n" if $opt->{debug};
+  unless ($arg->parent eq $CPrAN::PRAAT) {
+    print STDERR "D: ", $arg->basename, " is not in $CPrAN::PRAAT\n" if $opt->{debug};
     return 0;
   }
+
+  unless ($arg->basename =~ /^plugin_/) {
+    print STDERR "D: ", $arg->basename, " is not properly named\n" if $opt->{debug};
+    return 0;
+  }
+
+  return 1;
 }
 
+=item installed()
+
+Returns a list of all installed Praat plugins. See I<is_plugin()> for the
+criteria they need to fulfill.
+
+    my @installed = installed();
+    print "$_\n" foreach (@installed);
+
+=cut
 sub installed {
   use Path::Class;
 
@@ -73,6 +152,15 @@ sub installed {
   return @installed;
 }
 
+=item known()
+
+Returns a list of all plugins known by B<CPrAN>. In practice, this is the list of
+plugins whose descriptors have been saved by C<cpran update>
+
+    my @known = known();
+    print "$_\n" foreach (@known);
+
+=cut
 sub known {
   use Path::Class;
 
@@ -81,6 +169,21 @@ sub known {
   } dir( $CPrAN::ROOT )->children;
 }
 
+=item yesno()
+
+Gets either a I<yes> or a I<no> answer from STDIN. As arguments, it first
+receives a reference to the options hash, followed by the default answer (ie,
+the answer that will be entered if the user simply presses enter).
+
+    my $opt = ( yes => 1 );            # Will automatically say 'yes'
+    print "Yes or no? [y/N] ";
+    if (yesno($opt, 'n')) { print "You said yes\n" }
+    else { print "You said no\n" }
+
+By default, responses matching /^y(es)?$/i are considered to be I<yes>
+responses.
+
+=cut
 sub yesno {
   my ($opt, $default) = @_;
 
@@ -92,5 +195,30 @@ sub yesno {
   $input = $default if ($input eq "");
   ($input =~ /^y(es)?$/i) ? return 1 : return 0;
 }
+
+=back
+
+=head1 AUTHOR
+
+José Joaquín Atria <jjatria@gmail.com>
+
+=head1 LICENSE
+
+Copyright 2015 José Joaquín Atria
+
+This module is free software; you may redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+L<App::Cmd>, L<YAML::XS>, L<CPrAN::Command::remove>, L<CPrAN::Command::search>,
+L<CPrAN::Command::update>, L<CPrAN::Command::upgrade>, L<CPrAN::Command::show>,
+L<CPrAN::Command::install>
+
+=head1 VERSION
+
+0.0.1
+
+=cut
 
 1;
--- scripts/cpran.pl
+++ scripts/cpran.pl
@@ -2,3 +2,169 @@
 
 use CPrAN;
 CPrAN->run;
+
+=encoding utf8
+
+=head1 NAME
+
+B<CPrAN> - A package manager for Praat
+
+=head1 SYNOPSIS
+
+    # Update the list of known plugins
+    cpran update
+    # Shows the entire list of known plugins
+    cpran search .*
+    # Search in the known plugin list for something
+    cpran search something
+    # Search in the installed plugin list for something
+    cpran search -i something
+    # Show the descriptor of a plugin by name
+    cpran show name
+    # Install a plugin by name
+    cpran install name
+    # Upgrade all plugins to their most recent version
+    cpran update
+    # Upgrade a plugin by name to its most recent version
+    cpran update name
+    # Remove a plugin by name from disk
+    cpran remove name
+
+=head1 DESCRIPTION
+
+B<cpran> is the main script for an App::Cmd application to search, install,
+remove and update Praat plugins.
+
+=over
+
+=item B<update>
+
+See L<CPrAN::Command::update> for the full documentation.
+
+=item B<search>
+
+See L<CPrAN::Command::search> for the full documentation.
+
+=item B<show>
+
+See L<CPrAN::Command::show> for the full documentation.
+
+=item B<install>
+
+See L<CPrAN::Command::install> for the full documentation.
+
+=item B<upgrade>
+
+See L<CPrAN::Command::upgrade> for the full documentation.
+
+=item B<remove>
+
+See L<CPrAN::Command::remove> for the full documentation.
+
+=back
+
+=head1 FILES
+
+=head2 The preferences directory
+
+B<CPrAN> needs read and write access to I<Praat>'s preferences directory. The
+exact location for this directory varies according to the platform, so B<CPrAN>
+will keep the path to it in C<$CPrAN::PRAAT>.
+
+Below are the default locations for the main supported platforms:
+
+=over
+
+=item I<UNIX>
+
+C<~/.praat-dir>
+
+=item I<Macintosh>
+
+C</Users/username/Library/Preferences/Praat/Prefs>
+
+=item I<Windows>
+
+C<C:\Documents and Settings\username\Praat>
+
+=back
+
+Where C<username> is, of course, the name of the active user.
+
+Functions like installed() and is_plugin() look in this directory to get their
+information.
+
+=head2 Plugin descriptors
+
+B<CPrAN> plugins are identified as such by the presence of a I<plugin
+descriptor> in the plugin's root. The descriptor (named C<cpran.yaml>) is a YAML
+file with fields that identify the name and version of the plugin, what it does,
+what its requirements are, etc.
+
+A commented example is bundled with this module as C<example.yaml>, but here is
+a version stripped of comments, for simplicity:
+
+    ---
+    Plugin: name
+    Homepage: https://example.com
+    Version: 1.2.3
+    Maintainer: A. N. Author <author@example.com>
+    Depends:
+      praat: 5.0.0+
+    Recommends:
+    License:
+      - GPL3 <https://www.gnu.org/licenses/gpl-3.0.html>
+      - MIT <http://opensource.org/licenses/MIT>
+    Readme: readme.md
+    Description:
+      Short: an example of a plugin descriptor
+      Long: >
+        This file serves as an example of a CPrAN plugin descriptor.
+
+        This long description is optional, but very useful to have. Line
+        breaks in the long description will be converted to spaces, but you
+        can start a new paragraph by using a blank line.
+
+        Like so.
+
+B<CPrAN> uses YAML::XS to attempt to parse the descriptor. Any error in parsing
+will be treated by B<CPrAN> in the same way as if the file was missing, so it's
+important to properly validate the descriptor beforehand.
+
+=head2 The plugin list
+
+To keep track of available plugins, B<CPrAN> keeps the descriptors for all the
+plugins it knows about, and queries them for information when necessary. This is
+the list that known() looks in, and the list from where the B<show> command gets
+its data.
+
+The descriptors are saved in a B<CPrAN> root folder whose path is stored
+internally in C<$CPrAN::ROOT>. By default, it will be a directory named
+C<.cpran> in the root of the B<CPrAN> plugin (C<$CPrAN::PRAAT/plugin_cpran>). In
+that directory, the descriptors are renamed with the name of the plugin they
+represent.
+
+This list is updated with the B<update> command.
+
+=head1 AUTHOR
+
+José Joaquín Atria <jjatria@gmail.com>
+
+=head1 LICENSE
+
+Copyright 2015 José Joaquín Atria
+
+This program is free software; you may redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+L<CPrAN>, L<CPrAN::Command::remove>, L<CPrAN::Command::search>, 
+L<CPrAN::Command::update>, L<CPrAN::Command::upgrade>, L<CPrAN::Command::show>,
+L<CPrAN::Command::install>
+
+=head1 VERSION
+
+0.0.1
+
+=cut
