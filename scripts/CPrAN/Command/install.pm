d69c003e57534f73d16c1214d86289333c3779a9
baboso <jjatria@gmail.com>
2015-03-22 12:37:11 +0000

    install: added more checks and comments, and --force

--- scripts/CPrAN/Command/install.pm
+++ scripts/CPrAN/Command/install.pm
@@ -13,8 +13,13 @@ binmode STDOUT, ':utf8';
 sub opt_spec {
   return (
     [ "yes|y", "do not ask for confirmation" ],
-    [ "debug",    "print debugging messages" ],
+    [ "force|f", "print debugging messages" ],
+    [ "debug", "force installation of plugin" ],
   );
+}
+
+sub description {
+  return "The initialize command prepares the application...";
 }
 
 sub validate_args {
@@ -43,14 +48,14 @@ sub execute {
   my %installed;
   $installed{$_} = 1 foreach (CPrAN::installed());
 
-  # Plugins that are already installed cannot be installed again (unless we
-  # want to re-install them, which might be necessary in the future).
+  # Plugins that are already installed cannot be installed again (unless the
+  # user orders a forced-reinstall).
   # @names will hold the names of the plugins passed as arguments that are
   #   a) valid CPrAN plugin names; and
-  #   b) not already installed
+  #   b) not already installed (unless the user forces reinstallation)
   my @names;
   foreach (@{$args}) {
-    if (exists $installed{$_}) {
+    if (exists $installed{$_} && !$opt->{force}) {
       warn "W: $_ is already installed\n";
     }
     else {
@@ -69,84 +74,88 @@ sub execute {
   # The dependencies() subroutine calls itself recursively to include the
   # dependencies of the dependencies, and so on.
   my @deps = dependencies($opt, \@names);
-#   print Dumper(\@deps);
 
   # The source tree is then ordered to get a schedule of plugin installation.
   # In the resulting list, plugins with no dependencies come first, and those
   # that depend on them come later.
   my @ordered = order_dependencies(@deps);
-#   print Dumper(\@ordered);
 
   # Scheduled plugins that are already installed need to be removed from the
   # schedule
+  # TODO(jja) What does --force mean in this context?
   my @schedule;
   foreach (@ordered) {
     push @schedule, $_ unless (exists $installed{$_->{name}});
   }
 
-  print "The following plugins will be INSTALLED:\n";
-  print '  ', join(' ', map { $_->{name} } @schedule), "\n";
-  print "Do you want to continue? [y/N] ";
-  if (CPrAN::yesno($opt, 'n')) {
-    foreach (@schedule) {
-      use GitLab::API::v3;
+  # Output and user query modeled after apt's
+  if (@schedule) {
+    print "The following plugins will be INSTALLED:\n";
+    print '  ', join(' ', map { $_->{name} } @schedule), "\n";
+    print "Do you want to continue? [y/N] ";
+    if (CPrAN::yesno($opt, 'n')) {
+      foreach (@schedule) {
+        use GitLab::API::v3;
 
-      print "Downloading archive for $_->{name}\n";
+        print "Downloading archive for $_->{name}\n";
 
-      # Now that we know what plugins to install and in what order, we get them
-      my $api = GitLab::API::v3->new(
-        url   => 'https://gitlab.com/api/v3/',
-        token => $CPrAN::TOKEN,
-      );
+        # Now that we know what plugins to install and in what order, we get them
+        my $api = GitLab::API::v3->new(
+          url   => 'https://gitlab.com/api/v3/',
+          token => $CPrAN::TOKEN,
+        );
 
-      my $project = shift $api->projects({ search => 'plugin_' . $_->{name} });
-      my $tag = shift $api->tags($project->{id});
+        my $project = shift $api->projects({ search => 'plugin_' . $_->{name} });
+        my $tag = shift $api->tags($project->{id});
 
-      my %params = ( sha => $tag->{commit}->{id} );
-  #     # BUG(jja) This should work, but the Perl GitLab API seems to currently be
-  #     # broken. See https://github.com/bluefeet/GitLab-API-v3/issues/5
-  #     my $archive = $api->archive(
-  #       $project->{id},
-  #       \%params
-  #     );
-  #     print Dumper(\%params);
+        my %params = ( sha => $tag->{commit}->{id} );
+  #       # HACK(jja) This should work, but the Perl GitLab API seems to currently be
+  #       # broken. See https://github.com/bluefeet/GitLab-API-v3/issues/5
+  #       my $archive = $api->archive(
+  #         $project->{id},
+  #         \%params
+  #       );
 
-      # BUG(jja) This is a workaround while the Perl GitLab API is fixed
-      use LWP::Curl;
-      use File::Temp;
-      use Path::Class;
-      use Archive::Extract;
+        # HACK(jja) This is a workaround while the Perl GitLab API is fixed
+        use LWP::Curl;
+        use File::Temp;
+        use Path::Class;
+        use Archive::Extract;
 
-      my $referer = '';
-      my $get_url = 'http://gitlab.com/api/v3/projects/' . $project->{id} . '/repository/archive?private_token=' . $CPrAN::TOKEN . '&sha=' . $params{sha};
-      my $lwpcurl = LWP::Curl->new();
-      my $content = $lwpcurl->get($get_url, $referer);
+        my $referer = '';
+        my $get_url = 'http://gitlab.com/api/v3/projects/' . $project->{id} . '/repository/archive?private_token=' . $CPrAN::TOKEN . '&sha=' . $params{sha};
+        my $lwpcurl = LWP::Curl->new();
+        my $content = $lwpcurl->get($get_url, $referer);
 
-      my $tmp = File::Temp->new(
-        dir => '.',
-        template => 'tempXXXXX',
-        suffix => '.tar.gz',
-      );
-      my $archive = file($tmp->filename);
-      my $fh = $archive->openw();
-      $fh->print($content);
-      print "D: Wrote to $archive\n" if $opt->{debug};
-      my $ae = Archive::Extract->new( archive => $tmp->filename );
-      my $ok = $ae->extract( to => $CPrAN::PRAAT )
-        or die "Could not extract package: $ae->error";
+        my $tmp = File::Temp->new(
+          dir => '.',
+          template => 'tempXXXXX',
+          suffix => '.tar.gz',
+        );
+        my $archive = file($tmp->filename);
+        my $fh = $archive->openw();
+        $fh->print($content);
+        print "D: Wrote to $archive\n" if $opt->{debug};
+        my $ae = Archive::Extract->new( archive => $tmp->filename );
+        my $ok = $ae->extract( to => $CPrAN::PRAAT )
+          or die "Could not extract package: $ae->error";
 
-      # GitLab archives have a ".git" suffix in their directory names
-      # We need to remove that suffix
-      use File::Copy;
-      my $dir = dir( $CPrAN::PRAAT, 'plugin_' . $_->{name} );
-      move($ae->extract_path, $dir);
+        # GitLab archives have a ".git" suffix in their directory names
+        # We need to remove that suffix
+        use File::Copy;
+        my $dir = dir( $CPrAN::PRAAT, 'plugin_' . $_->{name} );
+        move($ae->extract_path, $dir);
+      }
     }
-  }
-  else {
-    print "Abort.\n";
+    else {
+      print "Abort.\n";
+    }
   }
 }
 
+# Query the desired plugins for dependencies
+# Takes either the name of a single plugin, or a list of names, and returns
+# an array of hashes properly formatted for processing with order_dependencies()
 sub dependencies {
   my ($opt, $args) = @_;
 
@@ -168,7 +177,8 @@ sub dependencies {
       my $descriptor = read_file($file->stringify);
       my $yaml = YAML::XS::Load( $descriptor );
 
-      # HACK(jja) Only consider CPrAN dependencies
+      # HACK(jja) Only consider CPrAN dependencies and delete the "cpran/"
+      # prefix in the dependency list
       my @deps;
       foreach my $dep (keys %{$yaml->{Depends}}) {
         if ($dep =~ /^cpran/) {
@@ -177,7 +187,8 @@ sub dependencies {
         }
       }
 
-      # HACK(jja) We need to restore the stripped "cpran/" prefix
+      # HACK(jja) We need to restore the stripped "cpran/" prefix so we can
+      # recognize them later
       my @vers = map {
         $yaml->{Depends}->{'cpran/' . $_};
       } @deps;
@@ -197,6 +208,12 @@ sub dependencies {
   return @dependencies;
 }
 
+# Order required packages, so that those that are depended up come up first than
+# those that depend on them
+# The argument is an array of hashes, each of which needs a "name" key that
+# identifies the item, and a "requires" holding the reference to an array with
+# the names of the items that are required.
+# Closely modeled after http://stackoverflow.com/a/12166653/807650
 sub order_dependencies {
   use Graph qw();
 
