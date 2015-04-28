da2b18d2d2ae1aa8478917866e3081fdf3ee6766
jjatria <jjatria@gmail.com>
2015-04-28 20:28:06 +0100

    Tiny API now uses Params::Validate. Less tiny now? :(

--- API/GitLab/Tiny.pm
+++ API/GitLab/Tiny.pm
@@ -3,47 +3,64 @@ package API::GitLab::Tiny;
 use strict;
 use warnings;
 
-# use Params::Validate;
-use Data::Dumper;
+use Params::Validate qw(:all);
 use Carp;
 
 sub new {
   my $class = shift;
-  my %args = @_;
-
-  exists ($args{token}) or die;
-  my $self = {};
-  $self->{url}      = $args{url} // 'https://gitlab.com/api/v3/';
-  $self->{token}    = $args{token};
-  $self->{debug}    = $args{debug} // 0;
-  $self->{encoding} = $args{encoding} // 'utf-8';
-  bless $self, $class;
+   bless { validate( @_, {
+    token    => { type => SCALAR },
+    url      => { type => SCALAR,  default => 'https://gitlab.com/api/v3/' },
+    debug    => { type => BOOLEAN, default => 0 },
+    encoding => { type => SCALAR,  default => 'utf-8' },
+  } ) }, $class;
 }
 
 sub projects {
-  my ($self, $params) = @_;
+  my $self = shift;
+  my ($params) = validate_pos( @_,
+    { type => HASHREF, optional => 1 }
+  );
 
   my $url = $self->_build_url( ['projects'], $params);
   return $self->_serialize( $self->_get($url) );
 }
 
 sub tags {
-  my ($self, $id, $params) = @_;
+  my $self = shift;
+
+  my ($id, $params) = validate_pos( @_,
+    { type => SCALAR },
+    { type => HASHREF, optional => 1 }
+  );
 
   my $url = $self->_build_url( ['projects', $id, 'repository', 'tags'], $params);
   return $self->_serialize( $self->_get($url) );
 }
 
 sub tree {
-  my ($self, $id, $params) = @_;
+  my $self = shift;
+
+  my ($id, $params) = validate_pos( @_,
+    { type => SCALAR },
+    { type => HASHREF, optional => 1 }
+  );
 
   my $url = $self->_build_url( ['projects', $id, 'repository', 'tree'], $params);
   return $self->_serialize( $self->_get($url) );
 }
 
 sub blob {
+  my $self = shift;
+
+  my ($id, $sha, $params) = validate_pos( @_,
+    { type => SCALAR },
+    { type => SCALAR },
+    { type => HASHREF, optional => 1 }
+  );
+
   use Encode qw(encode decode);
-  my ($self, $id, $sha, $params) = @_;
+
   $self->_require(['filepath'], $params);
 
   my $url = $self->_build_url( ['projects', $id, 'repository', 'blobs', $sha], $params);
@@ -51,15 +68,28 @@ sub blob {
 }
 
 sub raw_blob {
+  my $self = shift;
+
+  my ($id, $sha, $params) = validate_pos( @_,
+    { type => SCALAR },
+    { type => SCALAR },
+    { type => HASHREF, optional => 1 }
+  );
+
   use Encode qw(encode decode);
-  my ($self, $id, $sha, $params) = @_;
 
   my $url = $self->_build_url( ['projects', $id, 'repository', 'raw_blobs', $sha], $params);
   return encode($self->{encoding}, $self->_get($url), Encode::FB_CROAK);
 }
 
 sub file {
-  my ($self, $id, $params) = @_;
+  my $self = shift;
+
+  my ($id, $params) = validate_pos( @_,
+    { type => SCALAR },
+    { type => HASHREF, optional => 1 }
+  );
+
   $self->_require(['file_path', 'ref'], $params);
 
   my $url = $self->_build_url( ['projects', $id, 'repository', 'files'], $params);
@@ -67,59 +97,101 @@ sub file {
 }
 
 sub commits {
-  my ($self, $id, $params) = @_;
+  my $self = shift;
+
+  my ($id, $params) = validate_pos( @_,
+    { type => SCALAR },
+    { type => HASHREF, optional => 1}
+  );
 
   my $url = $self->_build_url( ['projects', $id, 'repository', 'commits'], $params);
   return $self->_serialize( $self->_get($url) );
 }
 
 sub commit {
-  my ($self, $id, $sha, $params) = @_;
+  my $self = shift;
+
+  my ($id, $sha, $params) = validate_pos( @_,
+    { type => SCALAR },
+    { type => SCALAR },
+    { type => HASHREF, optional => 1 }
+  );
 
   my $url = $self->_build_url( ['projects', $id, 'repository', 'commits', $sha], $params);
   return $self->_serialize( $self->_get($url) );
 }
 
 sub commit_diff {
-  my ($self, $id, $sha, $params) = @_;
+  my $self = shift;
+
+  my ($id, $sha, $params) = validate_pos( @_,
+    { type => SCALAR },
+    { type => SCALAR },
+    { type => HASHREF, optional => 1 }
+  );
 
   my $url = $self->_build_url( ['projects', $id, 'repository', 'commits', $sha, 'diff'], $params);
   return $self->_serialize( $self->_get($url) );
 }
 
 sub commit_comments {
-  my ($self, $id, $sha, $params) = @_;
+  my $self = shift;
+
+  my ($id, $sha, $params) = validate_pos( @_,
+    { type => SCALAR },
+    { type => SCALAR },
+    { type => HASHREF, optional => 1 }
+  );
 
   my $url = $self->_build_url( ['projects', $id, 'repository', 'commits', $sha, 'comments'], $params);
   return $self->_serialize( $self->_get($url) );
 }
 
 sub archive {
-  my ($self, $id, $params) = @_;
+  my $self = shift;
+
+  my ($id, $params) = validate_pos( @_,
+    { type => SCALAR },
+    { type => HASHREF, optional => 1 }
+  );
 
   my $url = $self->_build_url( ['projects', $id, 'repository', 'archive'], $params);
   return $self->_get($url);
 }
 
 sub groups {
-  my ($self, $params) = @_;
+  my $self = shift;
+
+  my ($params) = validate_pos( @_, 
+    { type => HASHREF, optional => 1 }
+  );
 
   my $url = $self->_build_url( ['groups'], $params);
   return $self->_serialize( $self->_get($url) );
 }
 
 sub group {
-  my ($self, $id, $params) = @_;
+  my $self = shift;
+
+  my ($id, $params) = validate_pos( @_,
+    { type => SCALAR },
+    { type => HASHREF, optional => 1 }
+  );
 
   my $url = $self->_build_url( ['groups', $id], $params);
   return $self->_serialize( $self->_get($url) );
 }
 
 sub _build_url {
+  my $self = shift;
+
+  my ($members, $params) = validate_pos( @_,
+    { type => ARRAYREF },
+    { type => HASHREF | UNDEF, optional => 1 }
+  );
+
   use URI;
   use URI::QueryParam;
-
-  my ($self, $members, $params) = @_;
 
   $params->{private_token} = $self->{token};
 
@@ -132,24 +204,33 @@ sub _build_url {
 }
 
 sub _serialize {
+  my $self = shift;
+
+  my ($content) = validate_pos( @_,
+    { type => SCALAR }
+  );
+
   require YAML::XS;
 
-  my ($self, $content) = @_;
   my $obj;
   eval {
     $obj = YAML::XS::Load( $content );
   };
   if ($@) {
-    croak "Could not parse: $@\n";
+    confess "Could not parse: $@\n";
   }
 
   return $obj;
 }
 
 sub _get {
-  require LWP::UserAgent;
-  my ($self, $url) = @_;
+  my $self = shift;
 
+  my ($url) = validate_pos( @_,
+    { type => SCALAR | SCALARREF }
+  );
+
+  require LWP::UserAgent;
   my $ua = LWP::UserAgent->new;
 
   my $response = $ua->get($url);
@@ -157,14 +238,20 @@ sub _get {
     return $response->decoded_content;
   }
   else {
-    croak $response->status_line;
+    confess $response->status_line;
   }
 }
 
 sub _require {
-  my ($self, $need, $have) = @_;
+  my $self = shift;
+
+  my ($need, $have) = validate_pos( @_,
+    { type => ARRAYREF },
+    { type => HASHREF }
+  );
+
   foreach (@{$need}) {
-    exists $have->{$_} or croak "No " . $_ . "provided";
+    exists $have->{$_} or confess "No " . $_ . "provided";
   }
 }
 
