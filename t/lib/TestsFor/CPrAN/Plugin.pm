5212cb65e69f1df0bb3aec479ca7cd6525556387
jjatria <jjatria@gmail.com>
2016-11-27 02:02:17 +0000

    Add more tests for CPrAN::Plugin

--- t/lib/TestsFor/CPrAN/Plugin.pm
+++ t/lib/TestsFor/CPrAN/Plugin.pm
@@ -23,16 +23,79 @@ sub test_attributes : Tests {
   # Public
   can_ok $test->class_name, $_
     foreach (qw( cpran version latest root is_cpran is_installed ));
+
+  # Methods
+  can_ok $test->class_name, $_
+    foreach (qw( is_cpran is_latest refresh remove ));
 }
 
 sub test_constructor : Tests {
   my $test = shift;
   my $class = $test->class_name;
   can_ok $class, 'new';
-  ok my $self = $class->new( name => '' ),
-    'constructor succeeds with name';
+
+  my $self;
+
+  ok $self = $class->new( name => '' ),
+    'constructor succeeds with empty name';
 
   isa_ok $self, $class;
+  is $self->name, '', 'Name is empty';
+
+  ok $self = $class->new( name => 'name' ),
+    'constructor succeeds with hash';
+  is $self->name, 'name', 'hash blessed to object';
+
+  ok $self = $class->new({ name => 'name' }),
+    'constructor succeeds with hashref';
+  is $self->name, 'name', 'hashref blessed to object';
+
+  ok $self = $class->new( 'name' ),
+    'constructor succeeds with scalar';
+  is $self->name, 'name', 'scalar set to name';
+}
+
+sub test_latest : Tests {
+  my $test = shift;
+  my $class = $test->class_name;
+
+  my $self = $class->new( 'test' );
+
+  is $self->is_latest, undef, 'is_latest undefined without remote';
+
+  $self->_remote({ version => '1.0.0' });
+  is $self->is_latest, 0, 'is_latest is false without local';
+
+  $self->_local({ version => '0.1.0' });
+  is $self->is_latest, 0, 'is_latest is false with lower version';
+
+  $self->_local({ version => '1.0.0' });
+  is $self->is_latest, 1, 'is_latest is true with equal versions';
+
+  $self->_local({ version => '1.1.0' });
+  is $self->is_latest, 1, 'is_latest is true with greater version';
+}
+
+sub test_remove : Tests {
+  my $test = shift;
+  my $class = $test->class_name;
+
+  my $self = $class->new( 'test' );
+
+  is $self->remove, undef, 'removed returns undefined with no root';
+
+  $self->root('/not/a/real/path');
+  is $self->remove, 0, 'removed returns false with missing root';
+
+  use Path::Tiny;
+  my $root = Path::Tiny->cwd->child('t', 'data', 'bad', 'plugin_' . $self->name);
+  $root->mkpath;
+  is $root->exists, 1, 'created root';
+
+  $self->root($root);
+  is  $self->remove, 1, 'removed returns true when deleting root';
+  is !$root->exists, 1, 'removed deleted root';
+  $root->remove;
 }
 
 # sub test_get : Tests {
